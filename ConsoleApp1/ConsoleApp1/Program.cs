eddbd0077f3e696ea7183efe7f9945fbd23b1744
Kuba <s15072@pjwstk.edu.pl>
2022-03-05 14:44:29 +0100

    init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var httpclient = new HttpClient();
+            var resp = await httpclient.GetAsync("https://pja.edu.pl");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
